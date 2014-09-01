e8cd0259e9243bccd20a2fd860a4f2ca101de9e1
Duan Wang <duanw@andrew.cmu.edu>
2014-09-01 15:27:11 -0400

    doesn't really work atm

--- js/ads.js
+++ js/ads.js
@@ -41,13 +41,11 @@ function onPlayerReady(event) {
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
-var done = false;
 function onPlayerStateChange(event) {
   console.log("Player state change");
-  if (event.data == YT.PlayerState.PLAYING && !done) {
+  if (event.data == YT.PlayerState.PLAYING) {
     var currentTime = player.getCurrentTime();
     //setTimeout(stopVideo, 6000);
-    done = true;
   }
 }
 function stopVideo() {
